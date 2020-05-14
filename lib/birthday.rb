41a1fbf9609d81c23ee6d76dc2dfd2137b391c9c
Tyler Firth <tyfirth@gmail.com>
2020-05-14 03:07:25 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,8 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age <= 12 puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if age <= 12 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
 end
 end
